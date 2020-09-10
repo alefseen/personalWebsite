9257cc81926c5ccb3a5c1f1449c6234870e396bb
alefseen <sooltaniyan@gmail.com>
2020-09-10 05:58:24 +0430

    add header section

---
+++ src/Components/About/About.js
@@ -0,0 +1,77 @@
+import React from "react";
+import "./About.scss";
+import Typed from "react-typed";
+
+function About() {
+  return (
+    <div className="aboutContainer">
+        <div className="introContent">
+        <div className="backText">
+        <div className="left">
+          من الف سین هستم
+        </div>
+        <div className="right">توسعه دهنده نرم افزار</div>
+        <div className="right">بزرگ شده توی کوچه های</div>
+        <div className="containBox"><span className="right">تهران،</span><span className="left"> ایران</span></div>
+        <div className="left">هر سایت یه شروعی داره</div>
+        <div className="right">این <span className="focusedText">شروع</span> منه!</div>
+      </div>
+      
+        </div>
+     <div className="projects">
+     salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>   salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>   salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>   salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>   salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>   salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>   salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>
+        salam
+        <br/>
+      </div>
+    </div>
+  );
+}
+
+export default About;
--- src/Components/App/Routing.js
+++ src/Components/App/Routing.js
@@ -1,15 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
-
+import About from "Components/About/About";
 function App() {
   return (
     <BrowserRouter basename="/">
       <Switch>
-        {/* <Route
-              path={process.env.PUBLIC_URL+"/"}
-              exact
-              component={Main}
-            /> */}
+        <Route path={process.env.PUBLIC_URL + "/"} exact component={About} />
       </Switch>
     </BrowserRouter>
   );
