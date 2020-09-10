bfc7e837e376c6589671235d43071b62f50fddd8
alefseen <sooltaniyan@gmail.com>
2020-09-10 18:04:24 +0430

    fix animation bug

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -5,22 +5,40 @@ import "./About.scss";
 function About() {
   return (
     <div className="aboutContainer">
-        <div className="introContent">
+      <div className="introContent">
         <div className="backText">
-        <div className="left">
-          من امیر سلطانیان هستم
+          <div>
+            <span className="left">من امیر سلطانیان هستم</span>
+          </div>
+          <div>
+            <span className="right">توسعه دهنده نرم افزار</span>
+          </div>
+          <div>
+            <span className="right">بزرگ شده توی کوچه های</span>
+          </div>
+          <div className="containBox">
+            <span className="right">
+              تهران،&nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
+            </span>
+            <span className="left">
+              &nbsp;&nbsp;&nbsp; &nbsp; &nbsp;&nbsp; ایران
+            </span>
+          </div>
+          <div>
+            <span className="left">هر سایت یه شروعی داره</span>
+          </div>
+          <div>
+            <span className="right">
+              این <span className="focusedText">شروع</span> منه!
+            </span>
+          </div>
         </div>
-        <div className="right">توسعه دهنده نرم افزار</div>
-        <div className="right">بزرگ شده توی کوچه های</div>
-        <div className="containBox"><span className="right">تهران،</span><span className="left"> ایران</span></div>
-        <div className="left">هر سایت یه شروعی داره</div>
-        <div className="right">این <span className="focusedText">شروع</span> منه!</div>
       </div>
-      
-        </div>
-     <div className="projects">
-     <span className="alert">به زودی تکمیل خواهم شد!</span>
-     <a href="tel:+989388856602"><span className="phone">+989388856602</span></a>
+      <div className="projects">
+        <span className="alert">به زودی تکمیل خواهم شد!</span>
+        <a href="tel:+989388856602">
+          <span className="phone">+989388856602</span>
+        </a>
       </div>
     </div>
   );
